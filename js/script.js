ee8c593b8854bbc30d0531cce055698db4223188
puncoviy <usaspinner@gmail.com>
2022-07-02 22:09:32 +0300

    feat: add slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const menuLink = document.querySelectorAll('.header-menu__item-link');
+const sliderDots = document.querySelectorAll('.dot');
+const sliderImg = document.querySelector('.header-slider__img');
 
 for (let i = 0; i<menuLink.length; i++) {
     menuLink[i].addEventListener('click', function(){
         menuLink.forEach(link => link.classList.remove('active'));
         menuLink[i].classList.add('active');
     });
+}
+
+for(let i=0; i<sliderDots.length; i++) {
+    sliderDots[i].addEventListener('click', function(){
+        sliderDots.forEach(dot => dot.classList.remove('active'));
+        sliderDots[i].classList.add('active');
+        sliderImg.src = './img/office' + (i+1) + '.png';
+    })
 }
 
---
+++ js/slider.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.header-slider').slick({
+        arrows: false,
+        // vertical: true,
+        // verticalSwiping: true,
+    });
+});
