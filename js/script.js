cbde28b0d7686ad26f84b3680d4903099a25677a
puncoviy <usaspinner@gmail.com>
2022-07-03 18:39:51 +0300

    feat: add custom slider(not fixed)

--- js/script.js
+++ js/script.js
@@ -9,11 +9,52 @@ for (let i = 0; i<menuLink.length; i++) {
     });
 }
 
+let imgSource = ['./img/office1.png', './img/office2.png', './img/office3.png', './img/office4.png', './img/office5.png'];
+let index = 0;
+
+setInterval (function(){
+    if (index === imgSource.length - 1) {
+      index = 0;
+    }
+    sliderImg.style.opacity = '0';
+    sliderDots.forEach(dot => dot.classList.remove('active'));
+    setTimeout(function(){
+        sliderImg.src = imgSource[index];
+    }, 300);
+    setTimeout(function(){
+        sliderImg.style.opacity = '1';
+        sliderDots[index].classList.add('active');
+    }, 300);
+    index++;
+} , 5000);
+
+sliderImg.addEventListener('click', function(){
+    if (index === imgSource.length - 1) {
+        index = 0;
+    };
+    sliderImg.style.opacity = '0';
+    sliderDots.forEach(dot => dot.classList.remove('active'));
+    setTimeout(function(){
+        sliderImg.src = imgSource[index];
+    }, 300);
+    setTimeout(function(){
+        sliderImg.style.opacity = '1';
+        sliderDots[index].classList.add('active');
+    }, 300);
+    index++;
+});
+
 for(let i=0; i<sliderDots.length; i++) {
     sliderDots[i].addEventListener('click', function(){
         sliderDots.forEach(dot => dot.classList.remove('active'));
         sliderDots[i].classList.add('active');
-        sliderImg.src = './img/office' + (i+1) + '.png';
+        sliderImg.style.opacity = '0';
+        setTimeout(function() {
+            sliderImg.src = './img/office' + (i+1) + '.png';
+            sliderImg.style.opacity = '1';
+        }, 300);
     })
 }
+
+
 
